4f7bac267c5abee4f01cfc306f7948244658669d
infrapuna <78515417+infrapuna@users.noreply.github.com>
2021-05-08 00:09:43 +0300

    fixed PORT

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const api = require("./crawler.js");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use("/", api);
 
